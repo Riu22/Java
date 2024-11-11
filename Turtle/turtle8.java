72c934bd9aee019ff39d6acf9abc31875c3bd34e
Riu22 <riutordjaume@gmail.com>
2024-11-11 11:07:59 +0100

    arreglos

--- Turtle/turtle8.java
+++ Turtle/turtle8.java
@@ -1,29 +1,21 @@
 public class turtle8{
     public static void main(String[] args) {
         Turtle t = new Turtle(500, 500);
-        t.goTo(0, 0);  // Coloca la tortuga en el centro
-
-        // Dibuja el símbolo templario
+        t.goTo(0, 0); 
         Templario(t);
-
         t.show();
     }
-
     public static void Templario(Turtle t) {
-        int tamañoTriangulo = 100; // Tamaño de los triángulos
-
-        // Dibuja los 4 triángulos
+        int tamañoTriangulo = 100; 
         for (int i = 0; i < 4; i++) {
             Triangulo(t, tamañoTriangulo);
-            t.turnRight(90);  // Gira 90 grados para dibujar el siguiente triángulo
+            t.turnRight(90);  
         }
     }
-
     public static void Triangulo(Turtle t, int tamaño) {
-        // Dibuja un triángulo apuntando hacia el centro
         for (int i = 0; i < 3; i++) {
-            t.forward(tamaño);  // Dibuja un lado del triángulo
-            t.turnRight(120);  // Gira 120 grados para el siguiente lado
+            t.forward(tamaño);  
+            t.turnRight(120);  
         }
     }
 }
