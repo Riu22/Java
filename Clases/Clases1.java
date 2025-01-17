26e6357a0a28561b9b2563e193d413f8cbc00ce7
Riu22 <riutordjaume@gmail.com>
2025-01-17 16:21:18 +0100

    Plantilla para el ejerccio 1

--- Clases/Clases1.java
+++ Clases/Clases1.java
@@ -1,4 +1,22 @@
 package Clases;
+import java.util.Scanner;
+class QuadratMagic{
+        QuadratMagic(int n){
+
+        }
+
+        void imprimeix{
+
+        }
+
+}
 
 public class Clases1 {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Quan grande quieres el cuadrado?; ");
+        int n = sc.nextInt();
+        QuadratMagic cuadrado = new QuadratMagic(n);
+    }
 }
+
